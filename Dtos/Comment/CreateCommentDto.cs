18dc3279fbf614c4616f4f0cf85fe06d4388866f
JoachimKvamme <kaj082024@gmail.com>
2024-10-21 11:01:55 +0200

    Data validation

--- Dtos/Comment/CreateCommentDto.cs
+++ Dtos/Comment/CreateCommentDto.cs
@@ -10,7 +10,12 @@ namespace api_tutorial.Dtos.Comment
     {
         [Required]
         [MinLength(5, ErrorMessage = "Title must be 5 characters or more")]
+        [MaxLength(280, ErrorMessage = "Title must be less than 280 characters long")]
         public string Title { get; set; } = string.Empty;
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Content must be 5 characters or more")]
+        [MaxLength(280, ErrorMessage = "Content must be less than 280 characters long")]
         public string Content { get; set; } = string.Empty;
 
     }
