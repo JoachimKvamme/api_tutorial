3fe36d4abbd357e18cde0cdd0e924e647b4eee4f
JoachimKvamme <kaj082024@gmail.com>
2024-10-21 14:54:39 +0200

    Identity confirmation, start

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -5,10 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using api_tutorial.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace api_tutorial.Data
 {
-    public class ApplicationDBContext : DbContext
+    public class ApplicationDBContext : IdentityDbContext<AppUser>
     {
         public ApplicationDBContext(DbContextOptions dbContextOptions)
         : base(dbContextOptions)
